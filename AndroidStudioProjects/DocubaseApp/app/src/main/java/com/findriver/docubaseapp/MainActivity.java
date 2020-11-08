1d68bfc37d718a27aedebc50bc0f909853e5ebf9
youba.mh <youba.mh@gmail.com>
2020-11-08 22:47:23 +0100

    implementation appcompat

--- AndroidStudioProjects/DocubaseApp/app/src/main/java/com/findriver/docubaseapp/MainActivity.java
+++ AndroidStudioProjects/DocubaseApp/app/src/main/java/com/findriver/docubaseapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.findriver.docubaseapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
